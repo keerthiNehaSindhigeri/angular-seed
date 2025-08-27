ebd5305fa741ebf7917c8d9c5eec7e8ee7de11cd
Keerthi <keerthi@cortiqa.in>
2025-08-27 15:36:14 +0530

    wip-dashboard

---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+  onButtonClick() {
+
+
+  }
+
+}
---
+++ src/app/modules/exceptions/exceptions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exceptions.component',
+  imports: [],
+  templateUrl: './exceptions.component.html',
+  styleUrl: './exceptions.component.scss'
+})
+export class ExceptionsComponent {
+
+}
